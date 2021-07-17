321e914b9abfd75acf7d1f0d44675a5debba69be
McKendree Strommer <mckendree.strommer@gmail.com>
2021-07-17 01:13:30 -0700

    committed for clarity

--- index.js
+++ index.js
@@ -1,68 +1,177 @@
 const inquirer = require('inquirer');
-
+const axios = require('axios');
 class Team {
   constructor(manager, engineerList, internList) {
     this.manager = manager;
     this.engineerList = engineerList;
     this.internList = internList;
   }
-  enterTeam() {
+  
+  async enterTeam() {
     //prompt for team, return answer data
-    function managerPrompt() {
-      inquirer
-        .prompt([
-          {
-            name: 'managerName',
-            type: 'input',
-            message: 'enter the manager of your team',
-          },
-          {
-            name: 'email',
-            type: 'input',
-            message: 'enter manager email',
-          },
-          {
-            name: 'ID',
-            type: 'input',
-            message: 'input manager employee ID',
-          },
-          {
-            name: 'office',
-            type: 'input',
-            message: 'input manager office number',
-          },
-        ])
-        .then((answer) => answer);
+    const managerPrompt = () => {
+          return inquirer
+            .prompt([
+              {
+                name: 'name',
+                type: 'input',
+                message: 'enter the manager of your team',
+              },
+              {
+                name: 'email',
+                type: 'input',
+                message: 'enter manager email',
+              },
+              {
+                name: 'id',
+                type: 'input',
+                message: 'input manager employee ID',
+              },
+              {
+                name: 'office',
+                type: 'input',
+                message: 'input manager office number',
+              },
+            ])
+            .then(
+              (answer) => answer,
+              (err) => {
+                throw new Error(err);
+              }
+            )
+        ;
     }
-    const manager = managerPrompt();
-    //TODO: This shit is off sync somehow
+
+    this.manager = new Manager(await managerPrompt());
+    this.internList = Intern.prototype.buildInternList();
+    this.engineerList = Engineer.prototype.buildEngineerList();
+    
   }
 }
+  
 
 class Manager {
-  constructor({ managerName }, { ID }, { email }, { office }) {
-    this.name = managerName;
-    this.ID = ID;
+  constructor({ name, id, email, office }) {
+    this.name = name;
+    this.id = id;
     this.email = email;
     this.office = office;
   }
 }
 
 class Engineer {
-  constructor(name, ID, email, gitHub) {
+  constructor({ name, id, email, gitHub }) {
     this.name = name;
-    this.ID = ID;
+    this.id = id;
     this.email = email;
     this.gitHub = gitHub;
+  }
+
+  buildEngineerList () {
+    const engineerList = [];
+
+    function engineerPrompt() {
+      inquirer
+        .prompt([
+          {
+            name: 'name',
+            type: 'input',
+            message: 'enter an engineer of your team',
+          },
+          {
+            name: 'email',
+            type: 'input',
+            message: 'enter engineer email',
+          },
+          {
+            name: 'id',
+            type: 'input',
+            message: 'input engineer employee ID',
+          },
+          {
+            name: 'gitHub',
+            type: 'input',
+            message: 'input engineer gitHub id',
+          },
+          {
+            name: 'another',
+            type: 'confirm',
+            message: 'enter another engineer?',
+          },
+        ])
+        .then(
+          (answer) => {
+            engineerList.push(new Engineer(answer));
+            console.log(engineerList);
+            if (answer.another) {
+              engineerPrompt();
+            }
+          },
+          (err) => {
+            throw new Error(err);
+          }
+        );
+    }
+    engineerPrompt();
+    return engineerList;
   }
 }
 
 class Intern {
-  constructor(name, ID, email, school) {
+  constructor({ name, id, email, school }) {
     this.name = name;
-    this.ID = ID;
+    this.id = id;
     this.email = email;
     this.school = school;
+  }
+
+  buildInternList() {
+    const internList = [];
+
+    function internPrompt() {
+      inquirer
+        .prompt([
+          {
+            name: 'name',
+            type: 'input',
+            message: 'enter an intern of your team',
+          },
+          {
+            name: 'email',
+            type: 'input',
+            message: 'enter intern email',
+          },
+          {
+            name: 'id',
+            type: 'input',
+            message: 'input intern employee ID',
+          },
+          {
+            name: 'school',
+            type: 'input',
+            message: 'input intern school',
+          },
+          {
+            name: 'another',
+            type: 'confirm',
+            message: 'enter another intern?',
+          },
+        ])
+        .then(
+          (answer) => {
+            internList.push(new Intern(answer));
+            console.log(internList);
+            if (answer.another) {
+              internPrompt();
+            }
+          },
+          (err) => {
+            throw new Error(err);
+          }
+        );
+    }
+    internPrompt();
+    return internList;
   }
 }
 
